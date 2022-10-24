2749aa154a9042c4ecea1545148b21a7dcedcb00
UcheOkonkwo <sirnuelcharles@gmail.com>
2022-10-24 14:34:12 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+/**
+ * _puts - ouput
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
+
+
+
+
+
+
+
+
+
