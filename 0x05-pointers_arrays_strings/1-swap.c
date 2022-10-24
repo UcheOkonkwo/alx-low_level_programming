2b1756daa8a06d7aeed46d42b28aefba2f9a61b6
UcheOkonkwo <sirnuelcharles@gmail.com>
2022-10-24 14:02:43 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * swap_int - reset to 98
+ * @a: pointer to swap
+ * @b: pointer to int
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
