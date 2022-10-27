1fdf3c560d7b0a156363b2a48b3c6a5e1c98cc3f
UcheOkonkwo <sirnuelcharles@gmail.com>
2022-10-27 08:28:23 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13.
+ * @s: int type array pointer
+ * Return: encoded OUTPUT value
+ */
+
+char *rot13(char *s)
+{
+int i;
+int j;
+char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (s[i] == input[j])
+{
+s[i] = output[j];
+break;
+}
+}
+}
+return (s);
+}
